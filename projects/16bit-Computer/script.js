5776097bf533c1156f46cf0ecdff290f38e74ea5
clhager <clhager@berkeley.edu>
2017-06-19 15:48:10 -0700

    Finished computer project

--- projects/16bit-Computer/script.js
+++ projects/16bit-Computer/script.js
@@ -58,3 +58,40 @@ function InstReg() {
 	document.getElementById('computer-part').innerHTML='<strong> Instruction Register: </strong>';
 	document.getElementById('computer-descrip').innerHTML=descrip;
 }
+
+function MIU() {
+	var descrip = "The MicroInstruction Unit is the link from the machine codes to the execution of the computer functions. \
+	The Unit takes in the 8-bit machine codes as inputs and executes a series of microinstructions corresponding to that \
+	machine code. It contains its own 3-bit counter to count through microinstructions, with a maximum of 8 time steps. \
+	Each time step, the Unit executes a microinstruction corresponding to the machine code at that time step, which through \
+	the pre-programmed ROM module activates the appropriate lines to enable input and/or output to the right parts of the \
+	computer architecture. As an optimization, each machine code microinstruction set ends with a reset step that resets the \
+	counter, so that the Unit doesn't go through empty time steps (as most machine codes only have 3-4 microinstructions. \
+	Find the microinstruction table <a href='MicroInstructions.pdf' target='_blank'>here</a> and the machine code  \
+	list <a href='Machine Code Table.pdf' target='_blank'>here</a>.";
+	document.getElementById('main_img').src='miu.png';
+	document.getElementById('computer-part').innerHTML='<strong> MicroInstruction Unit: </strong>';
+	document.getElementById('computer-descrip').innerHTML=descrip;
+}
+
+function RAM() {
+	var descrip = "The RAM module contains all the memory used by the program being executed to hold and assign \
+	variables, instantiate objects, and store any other needed information. The RAM module can be read from or \
+	written to using the appropriate machine codes. The RAM module also has a register in it, called the memory \
+	register, that indexes values at different parts of memory so that each function call in a recursive stack \
+	can access variables in its scope without having to know where it is in the recursive stack. The value in the memory \
+	register is automatically added any time the RAM is interacted with, so it can essentially set the address of the \
+	current 'scope'.";
+	document.getElementById('main_img').src='ram.png';
+	document.getElementById('computer-part').innerHTML='<strong> RAM Module: </strong>';
+	document.getElementById('computer-descrip').innerHTML=descrip;
+}
+
+function Output() {
+	var descrip = "The output register holds the values being sent through to the output decimal displays. Outside the output \
+	register, there is a set of Shift Add-3 modules that implement an algorithm for translating binary numbers into \
+	binary encoded decimal in order to communicate the correct value to the hexadecimal displays available in Logisim.";
+	document.getElementById('main_img').src='output.png';
+	document.getElementById('computer-part').innerHTML='<strong> Output Register: </strong>';
+	document.getElementById('computer-descrip').innerHTML=descrip;
+}
